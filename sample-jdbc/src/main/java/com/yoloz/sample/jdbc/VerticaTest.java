6699d75d082908b97282c0f63043d29deb0bf8a5
yoloz <z88897050@gmail.com>
2023-03-06 18:32:11 +0800

    add create table

--- sample-jdbc/src/main/java/com/yoloz/sample/jdbc/VerticaTest.java
+++ sample-jdbc/src/main/java/com/yoloz/sample/jdbc/VerticaTest.java
@@ -21,19 +21,20 @@ public class VerticaTest {
 //        Class.forName("com.vertica.jdbc.Driver");
         VerticaTest verticaTest = new VerticaTest();
 
-        String url = "jdbc:vertica://192.168.1.149:5433/test";
-        try (Connection conn = DriverManager.getConnection(url, "dbadmin", "vertica")) {
+        String url = "jdbc:vertica://192.168.124.234:5433/test_db";
+        try (Connection conn = DriverManager.getConnection(url, "dbadmin", "xxxxx")) {
 //            Util.getCatalogs(conn);
-//            Util.getSchemas(conn,"test","%");
-//            Util.getTables(conn, "test", "testsc", "%", null);
-//            Util.getColumns(conn,"test", "testsc", "person","%");
-            Util.getColumn(conn, "select * from test.testsc.typeTest");
-            verticaTest.simpleQuery(conn);
+//            Util.getSchemas(conn,"test_db","%");
+//            Util.getTables(conn, "test_db", "public", "%", null);
+//            Util.getColumns(conn,"test_db", "public", "person","%");
+//            Util.getColumn(conn, "select * from test_db.public.typeTest");
+            verticaTest.simpleUpdate(conn);
+//            verticaTest.simpleQuery(conn);
         }
     }
 
     public void simpleQuery(Connection connection) {
-        String sql = "select * from testsc.typeTest";
+        String sql = "select * from public.typeTest";
         try (Statement stmt = connection.createStatement()) {
             ResultSet resultSet = stmt.executeQuery(sql);
             Util.print(resultSet);
@@ -43,8 +44,29 @@ public class VerticaTest {
         }
     }
 
+    public void simpleUpdate(Connection connection) {
+        String sql = "CREATE TABLE public.typeTest\n" +
+                "(\n" +
+                "    int_t int,\n" +
+                "    varchar_t varchar(80),\n" +
+                "    timestamp_t timestamp,\n" +
+                "    float_t float,\n" +
+                "    varbinary_t varbinary(65000),\n" +
+                "    boolean_t boolean,\n" +
+                "    longbinary_t long varbinary(32000000),\n" +
+                "    longvarchar_t long varchar(65000),\n" +
+                "    uuid_t uuid\n" +
+                ");";
+        try (Statement stmt = connection.createStatement()) {
+            boolean resultSet = stmt.execute(sql);
+            System.out.println(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void inertTest(Connection connection) throws SQLException, IOException {
-        String sql = "INSERT INTO test.testsc.typeTest\n" +
+        String sql = "INSERT INTO test_db.public.typeTest\n" +
                 "(int_t, varchar_t, timestamp_t, float_t, varbinary_t, boolean_t, longbinary_t, longvarchar_t, uuid_t)\n" +
                 "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement pstmt = connection.prepareStatement(sql);
