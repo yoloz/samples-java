d95a74aed30232a00a4c22224a2e72be38ca5778
yoloz <z88897050@gmail.com>
2023-03-28 11:32:52 +0800

    for demo and test

--- cdh-hive/src/main/java/indi/yolo/sample/cdh/hive/Main.java
+++ cdh-hive/src/main/java/indi/yolo/sample/cdh/hive/Main.java
@@ -3,6 +3,8 @@ package indi.yolo.sample.cdh.hive;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -13,10 +15,33 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.kerberos();
+//        main.kerberos();
         main.noSasl();
-        main.userAndPwd();
+//        main.userAndPwd();
     }
+
+    static void print(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+        int colCount = resultSetMetaData.getColumnCount();
+        int total = 0;
+        StringBuilder header = new StringBuilder();
+        while (resultSet.next()) {
+            StringBuilder line = new StringBuilder();
+            for (int i = 1; i <= colCount; i++) {
+                if (total == 0) {
+                    header.append(resultSetMetaData.getColumnLabel(i)).append(" ");
+                }
+                line.append(resultSet.getObject(i)).append(',');
+            }
+            if (total == 0) {
+                System.out.println(header.toString());
+            }
+            System.out.println(line.substring(0, line.length() - 1));
+            total++;
+        }
+        System.out.println("==========" + total + "============");
+    }
+
 
     /**
      * com.cloudera.hive.jdbc41.HS2Driver
@@ -24,16 +49,17 @@ public class Main {
      */
     public void kerberos() {
         //principal: hive/cdh162@ZHDS.CO
-        String url = "jdbc:hive2://192.168.124.162:10000/default;AuthMech=1;" +
+        String url = "jdbc:hive2://192.168.124.162:10000/test;AuthMech=1;" +
                 "KrbRealm=ZHDS.CO;KrbHostFQDN=cdh162;" +
                 "KrbServiceName=hive;LogLevel=6;LogPath=/home/yoloz/kblog";
         try {
             Properties properties = new Properties();
             try (Connection conn = DriverManager.getConnection(url, properties);
                  Statement stmt = conn.createStatement();
-                 ResultSet rs = stmt.executeQuery("show databases")) {
+                 ResultSet rs = stmt.executeQuery("desc t1")) {
+//                System.out.println(conn.getCatalog() + "," + conn.getSchema());
                 while (rs.next()) {
-                    System.out.println(rs.getString(1));
+                    print(rs);
                 }
             }
         } catch (Exception e) {
