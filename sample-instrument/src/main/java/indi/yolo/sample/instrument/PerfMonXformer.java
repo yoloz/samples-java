8b57172c256d470f6e3fa14bd61c1fc047ace8b2
yoloz <z88897050@gmail.com>
2023-09-02 17:51:34 +0800

    Sat 02 Sep 2023 05:51:34 PM CST

--- sample-instrument/src/main/java/indi/yolo/sample/instrument/PerfMonXformer.java
+++ sample-instrument/src/main/java/indi/yolo/sample/instrument/PerfMonXformer.java
@@ -7,6 +7,24 @@ import javassist.ClassPool;
 import javassist.CtBehavior;
 import javassist.CtClass;
 
+/**
+ * CtClass cl=xx
+ * 添加Class字段：
+ * CtField newCtField = new CtField(pool.getCtClass("java/lang/ThreadLocal"), "threadLocal", cl);
+ * newCtField.setModifiers(Modifier.STATIC);
+ * 字段附上注解：
+ * final FieldInfo fieldInfo = newCtField.getFieldInfo();
+ * AnnotationsAttribute fieldAttr = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
+ * Annotation fieldAnno = new Annotation(NEW_FIELD_ANNOTATION_NAME, constPool);
+ * fieldAnno.addMemberValue(NEW_FIELD_ANNOTATION_VALUE,new StringMemberValue(fieldWrapper.sheetColumnValue, constPool));
+ * fieldAttr.addAnnotation(fieldAnno);
+ * fieldInfo.addAttribute(fieldAttr);
+ * 添加字段：
+ * cl.addField(threadLocal, CtField.Initializer.byNew(pool.getCtClass("java/lang/ThreadLocal")));
+ * 为字段添加getter和setter：
+ * ctClass.addMethod(CtNewMethod.getter(getter(fieldName), newCtField));
+ * ctClass.addMethod(CtNewMethod.setter(setter(fieldName), newCtField));
+ */
 public class PerfMonXformer implements ClassFileTransformer {
     public byte[] transform(ClassLoader loader, String className,
                             Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
